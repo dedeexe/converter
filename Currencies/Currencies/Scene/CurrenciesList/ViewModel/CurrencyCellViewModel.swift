b90257f309923fe3f112544f51655f88b3d49cd2
dede.exe <dede.exe@gmail.com>
2019-02-06 23:19:23 -0200

    add: ViewModel tests

--- Currencies/Currencies/Scene/CurrenciesList/ViewModel/CurrencyCellViewModel.swift
+++ Currencies/Currencies/Scene/CurrenciesList/ViewModel/CurrencyCellViewModel.swift
@@ -12,12 +12,18 @@ struct CurrencyViewViewModel : CurrencyViewDataSource {
     var name: String { return currency.name }
     
     var description: String {
-        guard let type = CurrencyType(rawValue: currency.name) else { return "Unknown" }
+        guard let type = CurrencyType(rawValue: currency.name) else {
+            return "Unknown"
+        }
+        
         return type.description
     }
     
     var image: UIImage? {
-        guard let type = CurrencyType(rawValue: currency.name) else { return nil }
+        guard let type = CurrencyType(rawValue: currency.name) else {
+            return nil
+        }
+        
         return type.image
     }
     
--- Currencies/CurrenciesTests/Scene/CurrenciesList/ViewModel/CurrencyCelViewModelTest.swift
+++ Currencies/CurrenciesTests/Scene/CurrenciesList/ViewModel/CurrencyCelViewModelTest.swift
@@ -14,27 +14,36 @@ class CurrencyCelViewModelTest: XCTestCase {
     var viewModel       : CurrencyViewViewModel!
     var currency        : CurrencyInfo!
     
-    let inputCode   = CurrencyType.CAD.rawValue
-    let inputValue  = Double(10.0)
-    let inputMult   = Double(1.0)
-    
-    override func setUp() {
-        
-        currency        = CurrencyInfo(name: inputCode , value: inputValue, multiplier: inputMult)
-        viewModel       = CurrencyViewViewModel(model: currency)
-        
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        let outputCode   = CurrencyType.CAD
+    func testValidCurrency() {
+        let inputCode   = CurrencyType.CAD.rawValue
+        let inputValue  = Double(10.0)
+        let inputMult   = Double(1.0)
+        let outputCode  = CurrencyType.CAD
   
+        let currency    = CurrencyInfo(name: inputCode , value: inputValue, multiplier: inputMult)
+        let viewModel   = CurrencyViewViewModel(model: currency)
+        
         XCTAssertEqual(viewModel.description, outputCode.description, "Wrong description")
         XCTAssertEqual(viewModel.currency, currency, "Wrong currency reference")
         XCTAssertEqual(viewModel.name, outputCode.rawValue, "Wrong name")
+        XCTAssertEqual(viewModel.image, outputCode.image, "Wrong image was returned")
     }
 
+    func testInvalidCurrency() {
+        let inputCode   = "XPTO"
+        let inputValue  = Double(10.0)
+        let inputMult   = Double(1.0)
+        let outputCode  = "XPTO"
+        let outputDesc  = "unknown"
+
+        let currency    = CurrencyInfo(name: inputCode , value: inputValue, multiplier: inputMult)
+        let viewModel   = CurrencyViewViewModel(model: currency)
+        
+        XCTAssertEqual(viewModel.description.lowercased(), outputDesc, "Wrong description")
+        XCTAssertEqual(viewModel.currency, currency, "Wrong currency reference")
+        XCTAssertEqual(viewModel.name.lowercased(), outputCode.lowercased(), "Wrong name")
+        XCTAssertNil(viewModel.image, "A nil value was expected for this situation")
+    }
+
+    
 }
